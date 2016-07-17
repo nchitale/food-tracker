c48bf5c25db7f4df2768b6c8cf07d52e9a3d5510
Nandini Chitale <nchitale@ucla.edu>
2016-07-17 16:28:57 -0700

    Change "MealTableViewController" name to "CardTableViewController"

--- FoodTracker/CardTableViewController.swift
+++ FoodTracker/CardTableViewController.swift
@@ -1,5 +1,5 @@
 //
-//  MealTableViewController.swift
+//  CardTableViewController.swift
 //  FoodTracker
 //
 //  Created by Nandini  on 7/6/16.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class MealTableViewController: UITableViewController {
+class CardTableViewController: UITableViewController {
     
     // MARK: Properties
     var meals = [Meal]()
